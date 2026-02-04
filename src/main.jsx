b8fb1d5ca367e8cd03a6ea9fd1e670615eb87f75
Samra <samra01734@gmail.com>
2026-02-04 04:41:38 -0800

    first day

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div className="min-h-screen flex items-center justify-center bg-yellow-400">
+      <h1 className="text-5xl font-bold text-black">
+        Tailwind Yellow BG Test ✅
+      </h1>
+    </div>
+  </React.StrictMode>
+)
